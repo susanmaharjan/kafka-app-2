95492baefbd0f1f22aeba84a14e64bf57b0e9611
susanmaharjan <susanmaharjan03030@gmail.com>
2020-10-04 15:19:02 -0500

    done

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # kafka-app-2
+## Prerequisities
+- OpenJDK
+- Apache Zookeeper
+- installed and working
+- Apache Kafka
+- installed and working
+## Start Zookeeper
+.\bin\windows\zookeeper-server-start.bat .\config\zookeeper.properties
+
+## Start Kafka
+.\bin\windows\kafka-server-start.bat .\config\server.properties
+
+## Create the Topic
+.\bin\windows\kafka-topics.bat --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --create --topic bucketlist
+
+## Compile and Build the Fat Jar File
+mvn clean compile assembly:single
+
+## Start Comsumer
+java -cp target/kafka-app-2-1.0-SNAPSHOT-jar-with-dependencies.jar edu.nwmissouri.susanmaharjan.Consumer bucketlist group1
+
+## Start Producer
+java -cp target/kafka-app-2-1.0-SNAPSHOT-jar-with-dependencies.jar edu.nwmissouri.susanmaharjan.Producer bucketlist
---
+++ src/main/java/edu/nwmissouri/susanmharjan/Producer.java
@@ -0,0 +1,51 @@
+package edu.nwmissouri.susanmaharjan;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+import java.util.Scanner;
+
+/**
+ * Created by sunilpatil on 12/28/15. Modified by Denise Case on 10/29/2019.
+ */
+public class Producer {
+  private static Scanner in;
+
+  public static void main(String[] argv) throws Exception {
+    if (argv.length != 1) {
+      System.err.println("Please specify 1 parameter (the name of the topic)");
+      System.exit(-1);
+    }
+    String topicName = argv[0];
+    in = new Scanner(System.in);
+    System.out.println("Thank you for providing the topic " + topicName + "\n");
+    System.out.println("Enter message (type exit to quit).\n");
+
+    // Configure the Producer
+    Properties configProperties = new Properties();
+    configProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    configProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+        "org.apache.kafka.common.serialization.ByteArraySerializer");
+    configProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+        "org.apache.kafka.common.serialization.StringSerializer");
+    System.out.println("The configuration properties are: " + configProperties.toString());
+    System.out.println("\nWill use this configuration to create a producer.\n");
+
+    org.apache.kafka.clients.producer.Producer producer = new KafkaProducer(configProperties);
+
+    // Make our own messages - create your custom logic here
+
+    String line = in.nextLine();
+    while (!line.equals("exit")) {
+        String todo = "I want to do "+line;
+      ProducerRecord<String, String> rec = new ProducerRecord<String, String>(topicName,todo);
+      producer.send(rec);
+      line = in.nextLine();
+    }
+
+    in.close();
+    producer.close();
+  }
+}
